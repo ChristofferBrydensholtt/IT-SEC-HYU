9b86fcdede001388b5d635510c6e4805db15ab59
ChristofferBrydensholtt <105556307+ChristofferBrydensholtt@users.noreply.github.com>
2023-03-30 16:54:37 +0900

    Add files via upload
    Without email function

---
+++ ss.py
@@ -0,0 +1,21 @@
+from PIL import ImageGrab
+from datetime import datetime
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+
+def screenshot():
+
+    now = datetime.now()
+
+    dt_string = now.strftime("%H.%M.%S")
+    snapshot = ImageGrab.grab()
+    save_path = "C:\\Users\\Christoffer\\Documents\\SecretScreenshots\\ss" +  dt_string + ".png"
+    snapshot.save(save_path)
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    screenshot()
